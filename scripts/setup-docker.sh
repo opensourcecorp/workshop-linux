110e626e727e164295c18e7a6e63ce2754b5d5c4
Justin DeBo <justin.debo@gmail.com>
2025-01-12 14:57:01 -0600

    Adding wetty with Traefik for reverse proxy

---
+++ scripts/setup-docker.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
+
+IP=$(hostname -I | awk '{print $1}')
+
+echo "$IP"
+#docker run --rm -d -p 3000:3000 wettyoss/wetty --ssh-host=$IP --title "DevUp Demo" --ssh-port=2332
